b3cd49245b1e78885f9cf6bf9aaf33af4073e592
*fQD1sd97jX <cookie.729@icloud.com>
2024-12-08 13:57:13 +0900

    feat: first push

---
+++ header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="https://unpkg.com/destyle.css@2.0.2/destyle.css">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <header class="site-header">
+    <div class="wrapper">
+      <h1 class="site-title">
+        <a href="#">
+          <h2>LUX</h2>
+        </a>
+      </h1>
+      <p class="site-description">キャンプやアウトドア</p>
+    </div>
+  </header>
+  <nav class="primary-navigation wrapper">
+    <button class="btn-menu">メニュー</button>
+    <ul class="menu-wrapper">
+      <li><a href="#"></a></li>
+      <li><a href="#">オーガニックフード</a></li>
+      <li><a href="#">インテリア</a></li>
+      <li><a href="#">アウトドアグッズ</a></li>
+      <li><a href="#">ヒーリング</a></li>
+    </ul>
+  </nav>
+  
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php get_header(); ?>
+<main class="main-contents wrapper">
+  <div class="post-list">
+    <article class="post-item">
+      <a href="#">aaaaaaaaaa</a>
+      <header class="post-header">
+        <h2 class="post-title">
+          <a href="#">初心者必携のキャンプグッズ</a>
+        </h2>
+        <time datetime="2024-12-08" class="post-date">2024年12月08日</time>
+        <ul class="post-categories">
+          <li><a href="#">オーガニック</a></li>
+        </ul>
+      </header>
+    </article>
+  </div>
+  <div class="nav-links">
+    <a href="#">←新しい投稿</a>
+    <a href="#">過去の投稿→</a>
+  </div>
+</main>
+<?php get_footer(); ?>
